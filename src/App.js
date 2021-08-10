b8e2b714027ae105739e769984196c9450ae5ad8
chandani <chandani.naikawadi@tectiq.in>
2021-08-10 10:23:44 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import logo from './logo.svg';
+import './App.css';
+import CarouselContainer from './components/CourselContainer';
+
+function App() {
+  return <CarouselContainer />;
+}
+export default App;
